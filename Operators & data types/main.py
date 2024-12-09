c1ba082217441d715d8796754d1019430fe71415
Donald8112$ <donald.kintu13@gmail.com>
2024-12-09 16:50:17 +0300

    operations in python

---
+++ Operators & data types/main.py
@@ -0,0 +1,15 @@
+name ="John" #string variable
+age= 16 #integer
+is_student= True #boolean
+weight= 48.6 #float
+print("Data type of name is:", type(name))
+print("Data type of age is:", type(age))
+print("Data type of is_student is:", type(is_student))
+print("Data type of weight is:", type(weight))
+print("######")
+age= str (age) #convert integer into string
+print("the new data type of age is:",type(age))
+weight= int (weight) #convert weight into string
+print("The new data typeof weight is:",type(weight))
+
+
---
+++ Operators & data types/main2.py
@@ -0,0 +1,17 @@
+a= 30
+b=5
+# arithemetic operations +-*/ %//**
+sum= a+b
+print("sum is :",sum)
+sub= a-b
+print("subtract is :",sub)
+mul= a*b
+print("multiply is :",mul)
+div= a/b
+print("divide is :",div)
+mod= a%b
+print("modulus is :",mod)
+floor= a//b
+print("floor is :",floor)
+ex= a**b
+print("exponential is :",ex)
