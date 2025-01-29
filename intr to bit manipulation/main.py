f7d521298a3fa5762508a0713fe96461cdaa95c1
Donald8112$ <donald.kintu13@gmail.com>
2025-01-29 16:33:55 +0300

    bit manipulation

---
+++ intr to bit manipulation/main.py
@@ -0,0 +1,16 @@
+num1= 10
+num2=4
+#AND OPERATOR
+print("Num1 AND Num2=",num1&num2)
+#OR OPERATOR
+print("Num1 OR Num2=",num1|num2)
+#XOR OPERATOR
+print("Num1 XOR Num2=",num1^num2)
+#NOT OPERATOR
+print("Not Num1=",~num1)
+#LEFT SHIFT OPERATOR
+print("Num1<<2=",num1<<2)
+#10011 <<2 =1001100 add 2 zeros at the end
+#RIGHT SHIFT OPERATOR
+print("Num1>>2=",num1>>2)
+#10011 >>2 =100 replace the last digit with zeros
